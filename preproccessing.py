93a3aca7db15edd767c6a61f0ed958b40ac203ad
ajana4096 <41270954+ajana4096@users.noreply.github.com>
2020-11-24 11:16:14 +0100

    Changing preproccessing from two modes to one

--- preproccessing.py
+++ preproccessing.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[6]:
 
 
 get_ipython().run_line_magic('matplotlib', 'inline')
@@ -23,7 +23,7 @@ from skimage import measure
 import cv2
 
 
-# In[2]:
+# In[10]:
 
 
 def get_area(rect):
@@ -31,17 +31,17 @@ def get_area(rect):
     return(w*h)
 
 
-# In[3]:
+# In[11]:
 
 
 def inverse(image):
     return (255-image)   
 
 
-# In[18]:
+# In[13]:
 
 
-def clean_image(img, black_and_white):
+def clean_image(img):
     img = img.astype('uint8')
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_clean = cv2.fastNlMeansDenoising(img,h=5) 
@@ -59,29 +59,43 @@ def clean_image(img, black_and_white):
     rects = sorted(rects,key=get_area,reverse=True)
     x,y,w,h = rects[0]
     img_to_show = img_clean[y:y+h,x:x+w]
-    if not black_and_white:
-        return img_to_show
+    #if not black_and_white:
+   #     return img_to_show
     avr = average(img_to_show)
     sd = std(img_to_show)
     if avr>200:
         ret,img = cv2.threshold(img_to_show, avr, 255,cv2.THRESH_BINARY_INV)
     elif avr>160:
-        img_to_show = inverse(img_to_show)
-        img_to_show = cv2.dilate(img_to_show, np.ones((3,3), np.uint8) , iterations=3) 
-        img_to_show = inverse(img_to_show)
-        ret,img = cv2.threshold(img_to_show, avr-1.5*sd, 255,cv2.THRESH_BINARY_INV)
+        img = inverse(img_to_show)
+        img = cv2.dilate(img, np.ones((3,3), np.uint8) , iterations=3) 
+        img = inverse(img)
+        ret,img = cv2.threshold(img, avr-1.5*sd, 255,cv2.THRESH_BINARY_INV)
     else:
-        img_to_show = inverse(img_to_show)
-        img_to_show = cv2.dilate(img_to_show, np.ones((5,5), np.uint8) , iterations=3) 
-        img_to_show = inverse(img_to_show)
-        ret,img = cv2.threshold(img_to_show, avr-1.3*sd, 255,cv2.THRESH_BINARY_INV)
+        img = inverse(img_to_show)
+        img = cv2.dilate(img, np.ones((5,5), np.uint8) , iterations=3) 
+        img = inverse(img)
+        ret,img = cv2.threshold(img, avr-1.3*sd, 255,cv2.THRESH_BINARY_INV)
     print("Średnia: "+str(avr)+" Odchylenie:"+str(sd))
-    return img
+    return img_to_show, img
 
 
-# In[7]:
+# In[5]:
 
 
 def clean_image_to_file(img, black_and_white,path):
-    cv2.imwrite(path, clean_image(img,black_and_white))
+    grayscale, thresholded = clean_image(img)
+    if black_and_white:
+        cv2.imwrite(path, thresholded)
+    else:
+        cv2.imwrite(path, grayscale)
+
+
+# In[18]:
+
+
+a,b = clean_image(cv2.imread("img/easy7.jpg"))
+plt.imshow(a, cmap='gray')
+plt.show()
+plt.imshow(b, cmap='gray')
+plt.show()
 
